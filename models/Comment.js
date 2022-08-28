234917ba681940fdd8e7086d54acc49ae8399522
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-28 10:00:39 +1000

    updated config and controllers

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Blogpost: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+       user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogpost',
+  }
+);
+
+module.exports = Blogpost;
---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./User');
+const Blogpost = require('./Blogpost');
+
+User.hasMany(Blogpost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blogpost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blogpost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blogpost };
