1bd4935203ce3a520fc57246ecf2d90bc6c0295b
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-09-15 09:19:33 +1000

    updated project route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
-
+const homeRoutes = require('../homeRoutes');
 router.use('/users', userRoutes);
-router.use('/projects', blogPostRoutes);
+router.use('/project', blogPostRoutes);
 router.use('/blogPostRoutes', blogPostRoutes);
 
 module.exports = router;
