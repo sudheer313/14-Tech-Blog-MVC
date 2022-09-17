8d9ae4ab9195b941b5ed3f357c7db13fe70bef50
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-09-17 17:59:24 +1000

    fixed project routs:

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
 const homeRoutes = require('../homeRoutes');
 router.use('/users', userRoutes);
-router.use('/project', blogPostRoutes);
+router.use('/projects', blogPostRoutes);
 router.use('/blogPostRoutes', blogPostRoutes);
 
 module.exports = router;
